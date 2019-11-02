bf2af28b3cf810eaf63600462246f8bc5a654fe5
Touseef90 <touseef.ghazanfar@gmail.com>
2019-11-02 09:32:22 +0500

    Work done till video 3 Classsed:

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to database');
+        return server.listen({ port: 5000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    });
